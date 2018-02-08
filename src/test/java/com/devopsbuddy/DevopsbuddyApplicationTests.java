f412f2d7320a51a8b2c825d931e32a3ea1957abc
Michael Jeszenka <mikeyjay39@gmail.com>
2018-02-08 13:46:08 +0100

    Enabled Spring security for the website

--- src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
+++ src/test/java/com/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -5,13 +5,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@WebAppConfiguration
+@WebAppConfiguration*/
 public class DevopsbuddyApplicationTests {
 
     @Autowired
