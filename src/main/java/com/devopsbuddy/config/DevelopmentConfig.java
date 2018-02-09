fbc065f42d585166589567fc0d72c5d0b162f2cf
Michael Jeszenka <mikeyjay39@gmail.com>
2018-02-09 14:25:37 +0100

    Set up the H2 console and created the repository layer

--- src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
+++ src/main/java/com/devopsbuddy/config/DevelopmentConfig.java
@@ -2,10 +2,14 @@ package com.devopsbuddy.config;
 
 import com.devopsbuddy.backend.service.EmailService;
 import com.devopsbuddy.backend.service.MockEmailService;
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.servlet.ServletRegistration;
 
 @Configuration
 @Profile("dev")
@@ -16,4 +20,12 @@ public class DevelopmentConfig {
     public EmailService emailService() {
         return new MockEmailService();
     }
+
+    @Bean
+    public ServletRegistrationBean h2ConsoleServletRegistration() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new WebServlet());
+        bean.addUrlMappings("/console/*");
+        return bean;
+    }
+
 }
--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -2,17 +2,22 @@ package com.devopsbuddy.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * Created by tedonema on 26/03/2016.
- */
+import java.util.Arrays;
+import java.util.List;
+
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment env;
 
     /** Public URLs. */
     private static final String[] PUBLIC_MATCHERS = {
@@ -24,10 +29,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/about/**",
             "/contact/**",
             "/error/**/*",
+            "/console/**"
     };
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        // This is required (disable csrf and frame options) from H2 console to work properly
+        List<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains("dev")) {
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+        }
+
         http
                 .authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
